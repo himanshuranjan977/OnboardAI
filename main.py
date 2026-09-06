a78fec1f3565b5e69c636e8e49fa39961128cc10
Himanshu Ranjan <himanshuranjan977.hitece2020@gmail.com>
2026-09-06 15:48:29 +0530

    Allow Render frontend in CORS

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://onboardai-frontend-o4sz.onrender.com"
     ],
 
     allow_credentials=True,
